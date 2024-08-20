0b5b081b0fee33700ed53e794f4a798d1f4f307f
snow.kwon <snow.kwon@toss.im>
2024-08-20 14:41:55 +0900

    ✨ Implement PoC

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,168 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import {
+  Button,
+  Container,
+  Divider,
+  Flex,
+  FormControl,
+  FormErrorMessage,
+  FormHelperText,
+  FormLabel,
+  Heading,
+  HStack,
+  IconButton,
+  Input,
+  InputGroup,
+  InputRightElement,
+  Textarea,
+  VStack,
+} from "@chakra-ui/react";
+import { CopyIcon, CheckIcon, AddIcon } from "@chakra-ui/icons";
+import { useCallback, useEffect, useMemo, useState } from "react";
+import { produce } from "immer";
+
+type Param = { key: string; value: string };
+const isNotBlank = (value: string) => value.length > 0;
+
+const parseUri = (uriString: string) => {
+  try {
+    const url = new URL(uriString);
+
+    return {
+      base: uriString.split("?")[0],
+      params: [...url.searchParams.entries()].map(([key, value]) => ({
+        key,
+        value,
+      })),
+    };
+  } catch {
+    return null;
+  }
+};
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [uriString, setUriString] = useState("supertoss://main?referrer=test");
+
+  const [uriBase, setUriBase] = useState(parseUri(uriString)?.base ?? "");
+  const [params, setParams] = useState<Param[]>(
+    parseUri(uriString)?.params ?? [],
+  );
+
+  const [copied, setCopied] = useState(false);
+
+  useEffect(() => {
+    setCopied(false);
+  }, [uriString]);
+
+  const addParam = () =>
+    setParams((params) => [...params, { key: "", value: "" }]);
+
+  const updateParams = (index: number, type: keyof Param) => (value: string) =>
+    setParams((params) =>
+      produce(params, (draftParams) => {
+        draftParams[index][type] = value;
+      }),
+    );
+
+  const removeParam = (index: number) =>
+    setParams((params) => params.filter((_, i) => i !== index));
+
+  const searchParams = useMemo(
+    () =>
+      new URLSearchParams(
+        params
+          .map((p) => [p.key, p.value])
+          .filter((entry) => entry.every(isNotBlank)),
+      ).toString(),
+    [params],
+  );
+
+  useEffect(() => {
+    setUriString(
+      uriBase + (isNotBlank(searchParams) ? `?${searchParams}` : ""),
+    );
+  }, [uriBase, setUriBase, searchParams]);
+
+  const onChangeUri = useCallback((uriString: string) => {
+    setUriString(uriString);
+
+    const uri = parseUri(uriString);
+
+    if (!uri) {
+      return;
+    }
+
+    setUriBase(uri.base);
+    setParams(uri.params);
+  }, []);
+
+  const error = useMemo(() => parseUri(uriString) === null, [uriString]);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <Container maxW="container.lg" pt={10}>
+      <Heading size="lg" mb={6}>
+        스킴 생성기
+      </Heading>
+      <FormControl isInvalid={error}>
+        <FormLabel>스킴</FormLabel>
+        <InputGroup>
+          <Input
+            value={uriString}
+            onChange={(e) => {
+              onChangeUri(e.target.value);
+            }}
+          />
+          <InputRightElement>
+            <IconButton
+              aria-label="copy url"
+              icon={copied ? <CheckIcon w={3} h={3} /> : <CopyIcon />}
+              onClick={() => {
+                navigator.clipboard.writeText(uriString);
+                setCopied(true);
+              }}
+            />
+          </InputRightElement>
+        </InputGroup>
+        {error && <FormErrorMessage>유효하지 않은 스킴이에요</FormErrorMessage>}
+      </FormControl>
+
+      <Divider my={10} />
+
+      <FormControl>
+        <FormLabel>기본 스킴</FormLabel>
+        <Input value={uriBase} onChange={(e) => setUriBase(e.target.value)} />
+      </FormControl>
+
+      <FormControl mt={4}>
+        <FormLabel>파라미터</FormLabel>
+
+        <VStack align="stretch">
+          {params.map((param, index) => (
+            <Flex key={index} gap={2}>
+              <Input
+                flex={1}
+                value={param.key}
+                onChange={(e) => updateParams(index, "key")(e.target.value)}
+              />
+              <Input
+                flex={2}
+                value={param.value}
+                onChange={(e) => updateParams(index, "value")(e.target.value)}
+              />
+            </Flex>
+          ))}
+
+          <Button onClick={addParam} leftIcon={<AddIcon w={3} h={3} />}>
+            파라미터 추가하기
+          </Button>
+        </VStack>
+      </FormControl>
+
+      <FormControl mt={10}>
+        <FormLabel>메모장</FormLabel>
+        <Textarea rows={8} />
+      </FormControl>
+    </Container>
+  );
 }
 
-export default App
+export default App;
